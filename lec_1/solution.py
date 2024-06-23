88b7df489acd5e489fc68d4d28cff44348b38f67
ShantKun <102741409+ShantKun@users.noreply.github.com>
2024-06-23 20:30:02 +0530

    solution.py

--- lec_1/solution.py
+++ lec_1/solution.py
@@ -1,4 +1,15 @@
 #write your answer here
+#Q1.T1
+x=input("Enter a number: ")
+print(x)
+count=0
+n=int(x)
+while(n!=0): 
+    n //= 10
+    count+=1
+print(count)
+
+
 #Q3.T1
 Computers ={
   "Laptop1" : {"brand" : "DELL","OS" : "Windows"},
@@ -13,12 +24,12 @@ for laptop, key in Computers.items():
    print(Brand)
    print(os)
 
-#Q1.T1
-x=input("Enter a number: ")
-print(x)
-count=0
-n=int(x)
-while(n!=0): 
-    n //= 10
-    count+=1
-print(count)
+
+#Leetcode
+class TWONUM(object):
+    def twoSum(self, nums, target):
+        for i in range(0,len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return[]
