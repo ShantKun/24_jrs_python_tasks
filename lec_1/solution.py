28897a8cc1631abe52f67090b4035b0d323ccc4f
ShantKun <102741409+ShantKun@users.noreply.github.com>
2024-06-23 20:20:09 +0530

    solution.py

--- lec_1/solution.py
+++ lec_1/solution.py
@@ -1,1 +1,14 @@
 #write your answer here
+#Q3.T1
+Computers ={
+  "Laptop1" : {"brand" : "DELL","OS" : "Windows"},
+  "Laptop2" : {"brand" : "HP", "OS" : "Linux"},
+  "Desktop" : {"brand" : "Apple","OS" : "Mac-OS"}
+}
+Brand=[]
+os=[]
+for laptop, key in Computers.items():
+   Brand.append(key["brand"])
+   os.append(key["OS"])
+   print(Brand)
+   print(os)
