b71928873ad787510ed10bf230528df43bca2cc5
ShantKun <102741409+ShantKun@users.noreply.github.com>
2024-06-25 21:35:23 +0530

    Q3.TASK1.py

--- Q3.TASK1.py
+++ Q3.TASK1.py
@@ -3,10 +3,7 @@ Computers ={
   "Laptop2" : {"brand" : "HP", "OS" : "Linux"},
   "Desktop" : {"brand" : "Apple","OS" : "Mac-OS"}
 }
-Brand=[]
-os=[]
-for laptop, key in Computers.items():
-   Brand.append(key["brand"])
-   os.append(key["OS"])
-   print(Brand)
-   print(os)
+Brand=[Computers["Laptop1"]["brand"],Computers["Laptop2"]["brand"],Computers["Desktop"]["brand"]]
+os=[Computers["Laptop1"]["OS"],computers["Laptop2"]["OS"],Computers["Desktop"]["OS"]]
+print(Brand)
+print(os)
